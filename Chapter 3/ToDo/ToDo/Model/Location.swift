1b7cc25cd427a42a9a6af34ef45046b4ba0563ff
vincenzo stira <v.stira@gmail.com>
2017-02-04 16:09:30 +0100

    A ToDo App with TDD

---
+++ Chapter 3/ToDo/ToDo/Model/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  ToDo
+//
+//  Created by Vincenzo Stira on 04/02/17.
+//  Copyright © 2017 Vincenzo Stira. All rights reserved.
+//
+
+import Foundation
+
+struct Location {
+  let name: String
+}
---
+++ Chapter 3/ToDo/ToDoTests/Model/ToDoItemTests.swift
@@ -0,0 +1,52 @@
+//
+//  ToDoItemTests.swift
+//  ToDo
+//
+//  Created by Vincenzo Stira on 04/02/17.
+//  Copyright © 2017 Vincenzo Stira. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo
+
+class ToDoItemTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+  func test_Init_WhenGivenTitle_SetsTitle(){
+    
+    let item  = ToDoItem(title: "Foo")
+    XCTAssertEqual(item.title, "Foo", "should set title")
+  }
+  
+  func test_Init_WhenGivenDescription_SetsDescription(){
+    
+    let item = ToDoItem(title: "", itemDescription: "Bar")
+    XCTAssertEqual(item.itemDescription, "Bar", "should set itemDescription")
+  }
+  
+  func test_Init_WhenGivenTimestamp_SetsTimestamp() {
+    
+    let item = ToDoItem(title: "", timestamp: 0.0)
+    XCTAssertEqual(item.timestamp, 0.0, "should set timestamp")
+  }
+  
+  func test_Init_WhenGivenLocation_SetsLocation() {
+  
+    let location = Location(name: "Foo")
+    let item = ToDoItem(title: "", location: location)
+    
+    XCTAssertEqual(item.location?.name, location.name, "should set location")
+  
+  }
+    
+    
+}
